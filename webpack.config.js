5b6a605ded5cc71d248fe1ce6126ece6bb142d61
Emre TINAZTEPE <emre@binalyze.com>
2020-07-30 21:03:55 +0300

    - Removed compression

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
 
 module.exports = {
   entry: path.join(__dirname, '/src/tigma.ts'),
@@ -25,12 +24,7 @@ module.exports = {
     extensions: [".ts", ".js"]
   },
   plugins: [
-    new CompressionPlugin({
-      compressionOptions: { level: 9 },
-      threshold: 10240,
-      minRatio: 0.8,
-      deleteOriginalAssets: false,
-    })
+
   ],
   devServer: {
     port: 8080,
